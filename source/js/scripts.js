896120b73725d6f02719484742824b48ca666b45
Varvara Bazhukova <mamasetta@gmail.com>
2019-08-25 22:44:33 +0500

    Мобильная версия главной страницы js

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,34 +1,63 @@
 var menuButton = document.querySelector(".header__toggle");
-var openMenu = document.querySelector(".js-header__toggle--open");
-var closeMenu = document.querySelector(".js-header__toggle--close");
 var mainNav = document.querySelector(".main-nav");
 var siteNav = document.querySelector(".site-nav");
+
+var closeMenu = function () {
+  menuButton.classList.remove("js-header__toggle--open");
+  menuButton.classList.add("js-header__toggle--close");
+  mainNav.classList.remove("js-hidden");
+  mainNav.classList.add("js-show");
+  siteNav.classList.remove("js-hidden");
+  siteNav.classList.add("js-show");
+}
+
+var openMenu = function () {
+  menuButton.classList.remove("js-header__toggle--close");
+  menuButton.classList.add("js-header__toggle--open");
+  mainNav.classList.remove("js-show");
+  mainNav.classList.add("js-hidden");
+  siteNav.classList.remove("js-show");
+  siteNav.classList.add("js-hidden");
+}
 
 menuButton.classList.add("js-show");
 menuButton.classList.add("js-header__toggle--open");
 mainNav.classList.add("js-hidden");
 siteNav.classList.add("js-hidden");
 
-openMenu.addEventListener("click", function(event) {
-  event.preventDefault();
-  menuButton.classList.remove("js-header__toggle--open");
-  menuButton.classList.add("js-header__toggle--close");
-
-  mainNav.classList.remove("js-hidden");
-  mainNav.classList.add("js-show");
-
-  siteNav.classList.remove("js-hidden");
-  siteNav.classList.add("js-show");
+menuButton.addEventListener("click", function (){
+  var menuIsOpen = menuButton.classList.contains("js-header__toggle--open");
+  menuIsOpen ?  closeMenu() : openMenu();
 });
 
-closeMenu.addEventListener("click", function(event) {
+var indexBusket = document.querySelector(".week-product__button");
+var busketModal = document.querySelector(".busket-modal");
+var busketWrapper = document.querySelector(".busket-modal__wrapper");
+
+indexBusket.addEventListener("click", function(event) {
   event.preventDefault();
-  menuButton.classList.remove("js-header__toggle--close");
-  menuButton.classList.add("js-header__toggle--open");
+  busketModal.classList.remove("js-hidden");
+  busketWrapper.classList.remove("js-hidden");
+  busketModal.classList.add("js-show");
+  busketWrapper.classList.add("js-show");
+});
 
-  mainNav.classList.remove("js-show");
-  mainNav.classList.add("js-hidden");
+busketWrapper.addEventListener("click", function(event) {
+  event.preventDefault();
+  busketModal.classList.remove("js-show");
+  busketWrapper.classList.remove("js-show");
+  busketModal.classList.add("js-hidden");
+  busketWrapper.classList.add("js-hidden");
+});
 
-  siteNav.classList.remove("js-show");
-  siteNav.classList.add("js-hidden");
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (busketModal.classList.contains("js-show") && busketModal.classList.contains("js-show")) {
+      busketModal.classList.remove("js-show");
+      busketWrapper.classList.remove("js-show");
+      busketModal.classList.add("js-hidden");
+      busketWrapper.classList.add("js-hidden");
+    }
+  }
 });
